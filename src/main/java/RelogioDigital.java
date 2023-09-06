60d224d3982ef2ab8338c712a0164bfb1e8da500
ViniciusCavalcantePequeno <144026431+ViniciusCavalcantePequeno@users.noreply.github.com>
2023-09-06 10:22:21 -0300

    RelogioDigital

---
+++ src/main/java/RelogioDigital.java
@@ -0,0 +1,38 @@
+class RelogioDigital {
+
+    private int hora;
+    private int minuto;
+
+    public RelogioDigital(){
+        this.hora = 12;
+        this.minuto = 20;
+    }
+    public int getHora(){
+        return this.hora;
+    }
+    public void setHora(int hora){
+        this.hora = hora;
+    }
+
+    public int getMinuto(){
+        return this.minuto;
+        
+    }
+    public void setMinuto(int minuto){
+        this.minuto = minuto;
+    }
+
+    public void tick(){
+        if (minuto == 59){
+            if (hora == 23){
+                hora = 0;
+                minuto = 0;}
+            else{
+                hora = hora + 1;
+                minuto = 0;
+            }}
+        else{
+            minuto = minuto + 1;
+        }
+    }
+}
